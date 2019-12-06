5de576955e898c00f8ce9a8a6fb00138f0b97e18
Ganbold Li <ganbold.li@gmail.com>
2019-12-06 03:49:55 -0800

    first commit

---
+++ api/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import json
+
+def switch_process(request):
+    # if request.method == 'POST':
+
+    return HttpResponse(json.dumps({"success": True}), content_type="application/json")
+
+def index(request):
+    return render(request, 'index.html', context=context)
