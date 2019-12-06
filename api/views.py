40298617c9d675463e4f2337d3734e05d7361fbd
Ganbold Li <ganbold.li@gmail.com>
2019-12-06 05:26:56 -0800

    process logic

--- api/views.py
+++ api/views.py
@@ -2,8 +2,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import json
 
+import subprocess
+import os, signal
+import time
+
+from psutil import process_iter
+from signal import SIGTERM
+
 def switch_process(request):
-    # if request.method == 'POST':
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        if data['action'] == "remote_control":
+            subprocess.call(['python3', '/home/pi/final/SunFounder_PiCar-V/remote_control/manage.py', 'runserver'])
+            subprocess.call(['/home/pi/ngrok', 'http', '8000', "-subdomain=remote-control.ngrok.io"])
+        elif data['action'] == "deep_pi_car":
+            for proc in process_iter():
+                for conns in proc.connections(kind='inet'):
+                    if conns.laddr.port == 8000:
+                        proc.send_signal(SIGTERM)
+            subprocess.call(['python3', '/home/pi/DeepPiCar/driver/code/deep_pi_car.py'])
+
+        return HttpResponse(json.dumps({"success": True, "action": data['action']}), content_type="application/json")
 
     return HttpResponse(json.dumps({"success": True}), content_type="application/json")
 
